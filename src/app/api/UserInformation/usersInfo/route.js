3cd0dac1c79ecf96ffa9500b0dc9de74cf26fb8f
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-10-01 18:32:42 +0530

    #

--- src/app/api/UserInformation/usersInfo/route.js
+++ src/app/api/UserInformation/usersInfo/route.js
@@ -8,18 +8,21 @@ import { verify } from "jsonwebtoken";
 export async function GET(req) {
     try {
         const authToken = req.cookies.get("authToken")
-        const userData = verify(authToken.value, process.env.AUTH_SECRETE_KEY);
-        if (userData) {
-            const userExistance = await getDoc(doc(db, "User", userData.uid))
-            if (userExistance.exists()) {
-                let fetchedUserData = {}
-                const userInfoSnap = await getDocs(collection(db, "User", userData.uid, "Information"))
-                const fetchPromise = new Promise((resolve, reject) => {
-                    userInfoSnap.forEach(doc => { fetchedUserData = { ...fetchedUserData, [doc.id]: doc.data() } })
-                    if (userInfoSnap.size === Object.keys(fetchedUserData).length) resolve();
-                })
-                await fetchPromise
-                return NextResponse.json({ status: 200, userData: fetchedUserData })
+        console.log(authToken)
+        if (authToken !== undefined) {
+            const userData = verify(authToken.value, process.env.AUTH_SECRETE_KEY);
+            if (userData) {
+                const userExistance = await getDoc(doc(db, "User", userData.uid))
+                if (userExistance.exists()) {
+                    let fetchedUserData = {}
+                    const userInfoSnap = await getDocs(collection(db, "User", userData.uid, "Information"))
+                    const fetchPromise = new Promise((resolve, reject) => {
+                        userInfoSnap.forEach(doc => { fetchedUserData = { ...fetchedUserData, [doc.id]: doc.data() } })
+                        if (userInfoSnap.size === Object.keys(fetchedUserData).length) resolve();
+                    })
+                    await fetchPromise
+                    return NextResponse.json({ status: 200, userData: fetchedUserData })
+                }
             }
         }
         return NextResponse.json({ status: 401 })
