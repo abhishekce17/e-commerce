678d6b2dde872f7b11982a11965a3bc991ef099c
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-10-01 18:29:00 +0530

    #

--- src/app/api/UserInformation/usersInfo/route.js
+++ src/app/api/UserInformation/usersInfo/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 import { db } from "@/firebase-config/config";
 import { getDoc, doc, collection, getDocs } from "firebase/firestore";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
 
 
-export async function GET() {
+export async function GET(req) {
     try {
-        const authToken = cookies().get("authToken")
+        const authToken = req.cookies.get("authToken")
         const userData = verify(authToken.value, process.env.AUTH_SECRETE_KEY);
         if (userData) {
             const userExistance = await getDoc(doc(db, "User", userData.uid))
