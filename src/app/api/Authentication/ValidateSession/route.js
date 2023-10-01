9a6104b25b67eac951484d4ce8be59e2c63645f6
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-10-01 15:30:40 +0530

    #

--- src/app/api/Authentication/ValidateSession/route.js
+++ src/app/api/Authentication/ValidateSession/route.js
@@ -8,7 +8,14 @@ export const dynamic = 'force-static';
 
 export async function GET(req) {
     try {
-        const authToken = cookies().get("authToken");
+        const cookieData = cookies().get("authToken")
+        const cookiePromise = new Promise((resolve) =>
+            setTimeout(() => {
+                resolve(cookieData)
+            }, 1000)
+        )
+        // const authToken = cookies().get("authToken");
+        const authToken = await cookiePromise();
         if (authToken === undefined) return NextResponse.json({ status: 401 })
         const userData = verify(authToken.value, process.env.AUTH_SECRETE_KEY);
         if (userData) {
