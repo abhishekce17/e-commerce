2e3426beb5f314012fcd87fc8b038a8724d8187d
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-10-02 23:56:46 +0530

    no cache

--- src/app/administrator/admin/category-managment/page.jsx
+++ src/app/administrator/admin/category-managment/page.jsx
@@ -34,7 +34,8 @@ const Page = () => {
 
   useEffect(() => {
     async function fetchCategories() {
-      const response = await fetch("/api/AdminCategories/FetchCategories", { next: { revalidate: 0 } })
+      // const response = await fetch("/api/AdminCategories/FetchCategories", { cache: "no-store" })
+      const response = await fetch("/api/AdminCategories/FetchCategories", { cache: "no-cache" })
       const resultData = await response.json()
       setCategories(resultData.data)
     }
