881f213a9986deaf5a70657c3efe272167b4d0e4
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-10-03 19:33:48 +0530

    #

--- src/app/api/add-products/route.js
+++ src/app/api/add-products/route.js
@@ -1,9 +1,9 @@
+"use server"
 import { NextResponse } from "next/server";
 import { db } from "@/firebase-config/config";
 import { collection, addDoc, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 const cloudinary = require('cloudinary');
 import cloudinary_config from "@/cloudinary-config/config";
-import streamifier from "streamifier"
 
 export const dynamic = "force-dynamic";
 export async function POST(req) {
