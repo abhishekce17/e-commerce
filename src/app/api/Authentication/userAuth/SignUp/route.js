a11c7111ab0a9c1e51fabd78d7a8846514298165
Abhishek Prajapati <91844013+abhishekce17@users.noreply.github.com>
2023-12-11 19:55:41 +0530

    verification feature removed for temporarly

--- src/app/api/Authentication/userAuth/SignUp/route.js
+++ src/app/api/Authentication/userAuth/SignUp/route.js
@@ -1,18 +1,18 @@
 // 'use server'
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, deleteUser } from "firebase/auth";
-import { NextResponse } from "next/server";
-import { db } from "@/firebase-config/config";
-import { sign } from 'jsonwebtoken';
-import { cookies } from "next/headers";
-import { collection, doc, setDoc, writeBatch } from "firebase/firestore";
+import {getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, deleteUser} from "firebase/auth";
+import {NextResponse} from "next/server";
+import {db} from "@/firebase-config/config";
+import {sign} from 'jsonwebtoken';
+import {cookies} from "next/headers";
+import {collection, doc, setDoc, writeBatch} from "firebase/firestore";
 
-const VERIFICATION_INTERVAL = 3000;
-const VERIFICATION_TIMEOUT = 60000;
+// const VERIFICATION_INTERVAL = 3000;
+// const VERIFICATION_TIMEOUT = 60000;
 
 export async function POST(req) {
     try {
         const auth = getAuth();
-        const { email, password, name, agreedTermAndCondition } = await req.json();
+        const {email, password, name, agreedTermAndCondition} = await req.json();
 
         const batch = writeBatch(db);
         let response;
@@ -27,70 +27,70 @@ export async function POST(req) {
         const personalInfoRef = doc(userCollection, "Personal");
 
         // Email verification
-        let isEmailVerified = false;
+        // let isEmailVerified = false;
 
-        await sendEmailVerification(response.user);
+        // await sendEmailVerification(response.user);
 
-        try {
-            const verificationPromise = new Promise((resolve, reject) => {
-                const verificationInterval = setInterval(async () => {
-                    console.log("Waiting for verification");
-                    const verifiedUser = await signInWithEmailAndPassword(auth, email, password);
+        // try {
+        //     const verificationPromise = new Promise((resolve, reject) => {
+        //         const verificationInterval = setInterval(async () => {
+        //             console.log("Waiting for verification");
+        // const verifiedUser = await signInWithEmailAndPassword(auth, email, password);
 
-                    if (verifiedUser.user.emailVerified) {
-                        isEmailVerified = true;
-                        clearInterval(verificationInterval);
-                        resolve();
-                    }
-                }, VERIFICATION_INTERVAL);
+        // if (verifiedUser.user.emailVerified) {
+        // isEmailVerified = true;
+        // clearInterval(verificationInterval);
+        // resolve();
+        // }
+        // }, VERIFICATION_INTERVAL);
 
-                setTimeout(() => {
-                    clearInterval(verificationInterval);
-                    resolve();
-                }, VERIFICATION_TIMEOUT);
-            });
-            await verificationPromise;
-        } catch (error) {
-            console.log(error)
-        }
+        // setTimeout(() => {
+        // clearInterval(verificationInterval);
+        // resolve();
+        // }, VERIFICATION_TIMEOUT);
+        // });
+        // await verificationPromise;
+        // } catch (error) {
+        // console.log(error)
+        // }
 
 
-        if (isEmailVerified) {
-            // User creation and data setup
-            await setDoc(doc(db, "User", response.user.uid), { createdAt: response.user.metadata.creationTime });
-            await setDoc(accountRef, { email, agreedTermAndCondition, emailVerify: true });
-            await setDoc(personalInfoRef, { fullName: name, contact: { email, phoneNo: null }, address: {}, wishlist: [] });
+        // if (isEmailVerified) {
+        // User creation and data setup
+        await setDoc(doc(db, "User", response.user.uid), {createdAt: response.user.metadata.creationTime});
+        await setDoc(accountRef, {email, agreedTermAndCondition, emailVerify: true});
+        await setDoc(personalInfoRef, {fullName: name, contact: {email, phoneNo: null}, address: {}, wishlist: []});
 
-            // Batch commit and token creation
-            await batch.commit()
-                .then(() => {
-                    const token = sign(user, process.env.AUTH_SECRETE_KEY);
-                    const isSecure = process.env.NODE_ENV === 'production';
+        // Batch commit and token creation
+        await batch.commit()
+            .then(() => {
+                const token = sign(user, process.env.AUTH_SECRETE_KEY);
+                const isSecure = process.env.NODE_ENV === 'production';
 
-                    // Set the token as an HttpOnly cookie
-                    cookies().set({
-                        name: 'authToken',
-                        value: token,
-                        httpOnly: true,
-                        secure: isSecure,
-                        sameSite: 'strict',
-                        path: '/',
-                        maxAge: 7 * 24 * 60 * 60
-                    });
-                })
-                .catch(e => {
-                    console.log(e);
-                    return NextResponse.json({ status: 500, error: "User creation failed" });
+                // Set the token as an HttpOnly cookie
+                cookies().set({
+                    name: 'authToken',
+                    value: token,
+                    httpOnly: true,
+                    secure: isSecure,
+                    sameSite: 'strict',
+                    path: '/',
+                    maxAge: 7 * 24 * 60 * 60
                 });
+            })
+            .catch(e => {
+                console.log(e);
+                return NextResponse.json({status: 500, error: "User creation failed"});
+            });
 
-            return NextResponse.json({ status: 201 });
-        } else {
-            console.log(response.user)
-            await deleteUser(response.user);
-            return NextResponse.json({ status: 500, error: "Email not Verified" });
-        }
+        return NextResponse.json({status: 201});
+        // } else {
+        //     console.log(response.user)
+        //     await deleteUser(response.user);
+        //     return NextResponse.json({ status: 500, error: "Email not Verified" });
+        // }
     } catch (error) {
         console.error("Error creating user:", error);
-        return NextResponse.json({ status: 500, error: "Something went wrong please try again leter" });
+        return NextResponse.json({status: 500, error: "Something went wrong please try again leter"});
     }
 }
--- src/app/authentication/sign-up/page.jsx
+++ src/app/authentication/sign-up/page.jsx
@@ -1,8 +1,8 @@
 "use client"
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import styles from "@/Styles/Authentication.module.css";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import UserAuthContext from "@/app/contextProvider";
 
 const SignUpForm = () => {
@@ -12,13 +12,13 @@ const SignUpForm = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [name, setName] = useState("");
   const [hasAgreed, setHasAgreed] = useState(false);
-  const [raiseError, setRaiseError] = useState({ confirmPasswordError: false, somethingWentWrongError: false })
+  const [raiseError, setRaiseError] = useState({confirmPasswordError: false, somethingWentWrongError: false})
   const [errorMessage, setErrorMessage] = useState(null)
-  const [timeInterval, setTimeInterval] = useState(false)
+  // const [timeInterval, setTimeInterval] = useState(false)
   const router = useRouter();
 
   const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
+    const {name, value, type, checked} = event.target;
 
     if (type === "checkbox") {
       setHasAgreed(checked);
@@ -36,18 +36,18 @@ const SignUpForm = () => {
   const createAccount = async (waitingTime) => {
     const accountCreationResponse = await fetch("/api/Authentication/userAuth/SignUp", {
       method: 'POST',
-      body: JSON.stringify({ email, password, name, agreedTermAndCondition: hasAgreed })
+      body: JSON.stringify({email, password, name, agreedTermAndCondition: hasAgreed})
     })
     const result = await accountCreationResponse.json()
     if (result.status === 201) {
       context.setIsUserLoggedIn(true)
       context.fetchUserData()
-      clearInterval(waitingTime)
+      // clearInterval(waitingTime)
       router.replace("/")
     }
     else {
-      clearInterval(waitingTime)
-      setRaiseError({ confirmPasswordError: false, somethingWentWrongError: true })
+      // clearInterval(waitingTime)
+      setRaiseError({confirmPasswordError: false, somethingWentWrongError: true})
       setErrorMessage(result.error)
     }
   }
@@ -56,27 +56,28 @@ const SignUpForm = () => {
   const validateUserSession = async () => {
     const existenceResponse = await fetch("/api/Authentication/userAuth/UserExistence", {
       method: "POST",
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({email, password})
     })
     const existenceResult = await existenceResponse.json()
     if (existenceResult.status === 200) {
-      setRaiseError({ ...raiseError, somethingWentWrongError: true })
+      setRaiseError({...raiseError, somethingWentWrongError: true})
       setErrorMessage("Eamil already exists")
     } else if (existenceResult.status === 401) {
-      setRaiseError({ somethingWentWrongError: false, confirmPasswordError: false })
-      setTimeInterval(60)
-      alert("a verification mail is send to your given mail id")
+      setRaiseError({somethingWentWrongError: false, confirmPasswordError: false})
+      // setTimeInterval(60)
+      // alert("a verification mail is send to your given mail id")
       createAccount()
-    } else if (existenceResponse.status === 429) {
-      setErrorMessage("Verification mail is already send to you")
-      setRaiseError({ ...raiseError, somethingWentWrongError: true })
     }
+    // else if (existenceResponse.status === 429) {
+    //   setErrorMessage("Verification mail is already send to you")
+    //   setRaiseError({ ...raiseError, somethingWentWrongError: true })
+    // }
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
-      setRaiseError({ ...raiseError, confirmPasswordError: true })
+      setRaiseError({...raiseError, confirmPasswordError: true})
       return
     }
     else {
@@ -174,16 +175,17 @@ const SignUpForm = () => {
         </div>
 
         <div className={styles.formField}>
-          <button className={styles.formFieldButton} disabled={timeInterval} > {timeInterval ? `waiting for varification ${timeInterval}` : "Sign Up"}</button>{" "}
+          {/* <button className={styles.formFieldButton} disabled={timeInterval} > {timeInterval ? `waiting for varification ${timeInterval}` : "Sign Up"}</button>{" "} */}
+          <button className={styles.formFieldButton} > {"Sign Up"}</button>
           <Link href="/authentication/sign-in" className={styles.formFieldLink}>
             I&apos;m already a member
           </Link>
         </div>
       </form>
-      {raiseError.somethingWentWrongError && <div style={{ color: "red" }} >
+      {raiseError.somethingWentWrongError && <div style={{color: "red"}} >
         {errorMessage}
       </div>}
-      {raiseError.confirmPasswordError && <div style={{ color: "red" }} >
+      {raiseError.confirmPasswordError && <div style={{color: "red"}} >
         password is not same
       </div>}
     </div>
